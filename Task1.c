1ee18f3f9c25087e48fff00839af8cfca09f2773
SherbanevIvan <ivan.sherbanev228@mail.ru>
2026-04-06 16:56:17 +0300

    complete Task1

---
+++ Task1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){ 
+
+    int N; 
+    scanf("%d", &N);
+
+    int *arr = (int*)malloc(N * sizeof(int)); 
+
+    for (int i = 0; i<N; i++){ 
+
+        printf("ВВедите число№%d ", i+1); 
+        scanf("%d", &arr[i]); 
+
+    }
+    printf("\n");
+
+    for (int i = 0; i < N; i++){ 
+
+        printf(" %d ", arr[i]);
+
+    }
+
+    printf("\n");
+    free(arr);
+
+}
